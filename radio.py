ff612a818bfe1dd93c268215bc1bb709fe20818c
yunusabd <yunusemin-github@yahoo.com>
2021-10-28 14:25:17 +0200

    cleanup, add naive timestamp in client

--- radio.py
+++ radio.py
@@ -1,82 +1,71 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import asyncio
-import websockets
-
+import io
+import os
 import requests
-import m3u8
-import subprocess
-
 import time
 import urllib3
 
-urllib3.disable_warnings()
+import m3u8
+import websockets
 
-start_time = time.time()
+from google.cloud import speech_v1p1beta1 as speech
 
 URL = "https://www.soundvideostar.com/live/_definst_/rti3/chunklist_w1129242241.m3u8"
 BASE = "https://www.soundvideostar.com/live/_definst_/rti3/"
+PHRASES = ["台灣", "中央廣播電台"]
 
-waiting = []
-started = []
+audio_files = []
 
-
-from google.cloud import speech_v1p1beta1 as speech
-import io
-
+urllib3.disable_warnings()
+start_time = time.time()
 client = speech.SpeechClient()
 
 
-async def stuff():
-    playlist = m3u8.load(URL, verify_ssl=False)
-    for line in playlist.dumps().splitlines():
-        if line.startswith("media") and line not in waiting and line not in started:
-            waiting.append(line)
-    print(round(time.time() - start_time, 1), "Finished getting new filenames")
-
-
-async def do_stuff_periodically(interval, periodic_function):
+async def get_filenames():
     while True:
-        print(round(time.time() - start_time, 1), "Getting filenames")
-        await asyncio.gather(
-            asyncio.sleep(interval),
-            periodic_function(),
-        )
+        playlist = m3u8.load(URL, verify_ssl=False)
+        for line in playlist.dumps().splitlines():
+            if line.startswith("media") and line not in audio_files:
+                audio_files.append(line)
+                r = requests.get(BASE + line, verify=False, stream=False)
+                with open("audio/" + line, "wb") as f:
+                    f.write(r.content)
+                    print("audio/" + line)
+                    yield "audio/" + line
+        await asyncio.sleep(5)
+        print(round(time.time() - start_time, 1), "Finished fetching new filenames")
 
 
 async def run_stt(websocket, path):
-    while True:
-        if len(waiting) == 0:
-            await asyncio.sleep(3)
-        for line in waiting:
-            started.append(line)
-            print(BASE + line)
-            r = requests.get(BASE + line, verify=False, stream=False)
+    async for filename in get_filenames():
+        with io.open(filename, "rb") as audio_file:
+            content = audio_file.read()
 
-            audio = speech.RecognitionAudio(content=r.content)
-            config = speech.RecognitionConfig(
-                encoding=speech.RecognitionConfig.AudioEncoding.MP3,
-                sample_rate_hertz=16000,
-                language_code="zh-TW",
-            )
+        audio = speech.RecognitionAudio(content=content)
 
-            response = client.recognize(config=config, audio=audio)
-
-            # Each result is for a consecutive portion of the audio. Iterate through
-            # them to get the transcripts for the entire audio file.
-            for result in response.results:
-                # The first alternative is the most likely one for this portion.
-                print(u"Transcript: {}".format(result.alternatives[0].transcript))
-                await websocket.send(result.alternatives[0].transcript)
-
-            print(r.status_code)
-            waiting.pop(waiting.index(line))
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.MP3,
+            sample_rate_hertz=16000,
+            language_code="zh-TW",
+            enable_automatic_punctuation=True,
+            speech_contexts=[speech.types.SpeechContext(phrases=PHRASES,)],
+        )
+        response = client.recognize(config=config, audio=audio)
+        print("Raw response: {}".format(response))
+        for result in response.results:
+            # The first alternative is the most likely one for this portion.
+            print(u"Transcript: {}".format(result.alternatives[0].transcript))
+            await websocket.send(result.alternatives[0].transcript)
 
 
 async def main():
+    if not os.path.exists("audio"):
+        os.makedirs("audio")
     async with websockets.serve(run_stt, "localhost", 8765):
-        task = asyncio.create_task(do_stuff_periodically(10, stuff))
-        await asyncio.Future()  # run forever
+        await asyncio.Future()
 
 
 asyncio.run(main())
