2f458dd7aadf71beea14db8d5fcc0594a06bf22c
yunusabd <yunusemin-github@yahoo.com>
2021-11-13 17:59:50 +0100

    wip

--- radio.py
+++ radio.py
@@ -11,6 +11,8 @@ import urllib3
 import m3u8
 import websockets
 
+from dotenv import load_dotenv
+import azure.cognitiveservices.speech as speechsdk
 from google.cloud import speech_v1p1beta1 as speech
 
 URL = "https://www.soundvideostar.com/live/_definst_/rti3/chunklist_w1129242241.m3u8"
@@ -21,7 +23,13 @@ audio_files = []
 
 urllib3.disable_warnings()
 start_time = time.time()
-client = speech.SpeechClient()
+client = None
+load_dotenv()
+
+AZURE_KEY = os.getenv("AZURE_KEY")
+AZURE_REGION = os.getenv("AZURE_REGION")
+
+SERVICE = "Azure"
 
 
 async def get_filenames():
@@ -39,26 +47,45 @@ async def get_filenames():
         print(round(time.time() - start_time, 1), "Finished fetching new filenames")
 
 
+async def Google(websocket, filename):
+    if not client:
+        client = speech.SpeechClient()
+    with io.open(filename, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
+        sample_rate_hertz=16000,
+        language_code="zh-TW",
+        enable_automatic_punctuation=True,
+        speech_contexts=[speech.types.SpeechContext(phrases=PHRASES,)],
+    )
+    response = client.recognize(config=config, audio=audio)
+    print("Raw response: {}".format(response))
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        await websocket.send(result.alternatives[0].transcript)
+
+
 async def run_stt(websocket, path):
     async for filename in get_filenames():
-        with io.open(filename, "rb") as audio_file:
-            content = audio_file.read()
+        if SERVICE == "Google":
+            await Google(websocket, filename)
+        elif SERVICE == "Azure":
+            speech_config = speechsdk.SpeechConfig(
+                subscription=AZURE_KEY, region=AZURE_REGION
+            )
+            audio_input = speechsdk.AudioConfig(filename=filename)
+            speech_recognizer = speechsdk.SpeechRecognizer(
+                speech_config=speech_config, audio_config=audio_input
+            )
 
-        audio = speech.RecognitionAudio(content=content)
-
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.MP3,
-            sample_rate_hertz=16000,
-            language_code="zh-TW",
-            enable_automatic_punctuation=True,
-            speech_contexts=[speech.types.SpeechContext(phrases=PHRASES,)],
-        )
-        response = client.recognize(config=config, audio=audio)
-        print("Raw response: {}".format(response))
-        for result in response.results:
-            # The first alternative is the most likely one for this portion.
-            print(u"Transcript: {}".format(result.alternatives[0].transcript))
-            await websocket.send(result.alternatives[0].transcript)
+            result = speech_recognizer.recognize_once_async().get()
+            print(result.text)
+            await websocket.send(result.text)
 
 
 async def main():
